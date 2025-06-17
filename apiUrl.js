bdc3d1789b7d0d6d64e2e3bbcd69a852de30d764
Bunty02-11 <buntshitman02@gmail.com>
2025-06-17 17:27:43 +0530

    ssr

--- apiUrl.js
+++ apiUrl.js
@@ -2,7 +2,7 @@
 export const apiUrl = "https://0vm9jauvgc.execute-api.us-east-1.amazonaws.com/stag/api";
 
 // Frontend base URL
-export const frontendUrl = "https://tornado-delta.vercel.app/"; // Replace with your actual domain
+export const frontendUrl = "https://tornado-livid.vercel.app/"; // Replace with your actual domain
 
 // API endpoints
 export const endpoints = {
